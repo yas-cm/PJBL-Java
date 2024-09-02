02abfe4c21de7c83f7eafdfca0cbfb17a536d543
Yas <yasminhayashi234@gmal.com>
2024-09-02 20:53:29 -0300

    Ate getters

--- Tutor.java
+++ Tutor.java
@@ -1,5 +1,46 @@
-private class Tutor{
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-
-
+public class Tutor{
+   // Atributos
+   private int codigo;
+   private String nome;
+   private String endereco;
+   private LocalDate dataNasc;
+   private ArrayList<Pet> pet=new ArrayList<Pet>();
+   
+   // Construtor
+   public Tutor(int c, String n, String e, LocalDate d){
+      codigo = c;
+      nome = n;
+      endereco = e;
+      dataNasc = d;
+   }
+   
+   // Getters
+   public int getCodigo() {return codigo;}
+   public String getNome() {return nome;}
+   public String getEndereco() {return endereco;}
+   public LocalDate getDataNasc() {return dataNasc;}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
