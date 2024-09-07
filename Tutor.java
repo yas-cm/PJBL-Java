d5e63aef4524fa1b0ceb67507c8f755e8320f8da
yas-cm <138698763+yas-cm@users.noreply.github.com>
2024-09-06 21:04:22 -0300

    Atualizado, agr tem a parte de imprimir

--- Tutor.java
+++ Tutor.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.Period;
 
 public class Tutor{
    // Atributos
@@ -22,14 +23,66 @@ public class Tutor{
    public int getCodigo() {return codigo;}
    public String getNome() {return nome;}
    public String getEndereco() {return endereco;}
-   public LocalDate getDataNasc() {return dataNasc;}
+   
+   public String getDataNasc() {
+      DateTimeFormatter fmt=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+      return dataNasc.format(fmt);
+   }
+   public int getIdade(){
+      LocalDate dataSistema=LocalDate.now();
+      int idade=Period.between(dataNasc,dataSistema).getYears();
+      return idade;
+   }
    
    // Adiciona pet
    public void incluiPet(String nomePet, String tipoPet){
       pet.add(new Pet(nomePet,tipoPet));
    }
    
-   // Falta a parte de juntar os dados de cada tutor em uma string pra poder imprimir mais pra frente
+   // Geração de string com os dados do tutor   
+   public String toString(){
+      String ts = String.format("Cod. do tutor: %d\n", codigo);
+      ts += String.format("  Nome...........: %s\n", nome);
+      ts += "  Data nascimento: "+getDataNasc()+ " ("+ getIdade() +" anos)\n";
+      ts += String.format("  Endereco.......: %s\n", endereco);
+      ts += "  Relacao de pets:\n";
+      if (pet.size() == 0)
+         ts+="Nenhum pet cadastrado.\n";
+      else{
+         for(Pet p:pet){ 
+            ts+= "  - Nome do pet: "+p.getNomePet()+"; ";
+            ts+= "Tipo: "+p.getTipoPet()+".\n";
+            }
+      }
+      return ts;
+      }
+}   
+
+// 
+// // pelo menos três tutores com seus pets
+// public static void popularCadastro() {
+//    Tutor t; // Objeto tutor.
+//    
+//    int codTut = geraCodTut(); // Cria código sequencial inicando em 1.
+//    t = new Tutor(codTut,"Josefina das Gracas");
+//    t.setD
+//    
+//    cont.add(c);
+//    System.out.println(c.getNome()+": cadastrado.");
+//    // Adiciona contribuinte 2 com dependente.
+//    codCont=geraCodCont(); // Cria próximo código sequencial.
+//    c=new Contribuinte(codCont,"Maria Cardoso");
+//    c.incluiDep("Pedro Cardoso",2,4,2012);
+//    cont.add(c);
+//    System.out.println(c.getNome()+": cadastrado.");
+//    // Adiciona contribuinte 3.
+//    codCont=geraCodCont(); // Cria próximo código sequencial.
+//    c=new Contribuinte(codCont,"Roberto Silva");
+//    cont.add(c);
+//    System.out.println(c.getNome()+": cadastrado.");
+// }
+//    
+//    
    
    
    
@@ -42,7 +95,3 @@ public class Tutor{
    
    
    
-   
-   
-   
-}
